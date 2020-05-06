0914e8de741128c438cbe560b5f9ae3b2a4f237c
silvnst <silvan.stoeckli@intergga.ch>
2020-05-06 14:41:03 +0200

    first codes

---
+++ challenge_1.R
@@ -0,0 +1,30 @@
+# Challenge 1
+
+
+
+chal <- read_file("challenge_1.txt")
+n <- str_count(chal)
+vec <- seq(1, round(n/40))
+func <- function(n){
+  pattern <- paste0("(.{", n, "}).*\\1")
+  str_detect(chal, pattern = pattern)
+}
+v1 <- sapply(vec, func)
+# length longest string
+l <- vec[max(which(v1==1))]
+
+guess <- str_extract(chal, pattern = paste0("(.{", l, "}).*\\1"))
+sol <- str_extract(guess, pattern = "^.{21}")
+sol
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ challenge_2.R
@@ -0,0 +1,13 @@
+# vigenere cipher
+# letters A-Z -> numbers 0-25
+
+# "If the letters A-Z are taken to be the numbers 0-25,
+# and addition is performed modulo 26, Vigenère encryption 
+# E using the key K can be written as 
+# C_i=E_K (M_i)=(M_i+K_i )mod 26 
+# M=M_1…M_n is the Message
+# C=C_1…C_n is the ciphertext
+# K=K_1… K_n is the key
+# obtained by repeating the keyword m times in 
+# which m is the keyword length."
+
